e994c972324d66798bad18390f1eb6b356f5a147
igorhsb <igorhsb37@gmail.com>
2022-02-04 19:57:12 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -35,5 +35,12 @@ public class Rental {
             break;
      }
      return amount;
-  }
+   }
+   
+   public int getFrequentRenterPoints() { 
+      if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) {
+         return 2;
+      }
+      return 1;
+   }
  }
