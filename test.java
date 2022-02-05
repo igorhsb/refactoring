e223b94ceb90c11a7a9ad1283af30b51dac35d5c
igorhsb <igorhsb37@gmail.com>
2022-02-04 21:00:42 -0300

    Commit 16

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ public class test {   // Classe utilizada para testes
     public static void main(String[] args) {
 
 		Customer m3 = new Customer("testName");
-		System.out.println(m3.statement());
+		System.out.println(m3.htmlStatement());
 
 	}
 }
